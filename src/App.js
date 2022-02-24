e871f4ee9af35f5266383531205498223cc981c0
Aida <aydahamedi56@gmail.com>
2022-02-24 11:35:40 -0800

    Added the react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather App 
+   <h1> Weather App </h1>
+    <footer>
+      This project was coded by Aida Hamedi and is{" "}
+      <a href="https://github.com/Aida2005/React-Weather-App" target="_blank">
+       open-sourced on GitHub
+
+      </a>
+      
+    </footer>
     </div>
   );
 }
